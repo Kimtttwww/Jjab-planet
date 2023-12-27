16d41fb6e23c3aba7ad3a23130aa994be90b359c
dysong08 <151178656+dysong08@users.noreply.github.com>
2023-12-27 17:38:39 +0900

    2023.12.27 퇴근전

---
+++ src/com/kr/corp/model/vo/Corporation.java
@@ -0,0 +1,30 @@
+package com.kr.corp.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Corporation {
+	
+
+		int corpNo;
+		String corpName;
+		String ceoName;
+		String corpBn;
+		String address;
+		int likeCount;
+		String homepage;
+		
+	
+	
+	
+	
+}
