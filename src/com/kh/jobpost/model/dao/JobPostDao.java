ae7c43b2978e9df7c2add03e39fe79ba590103b7
DESKTOP-4CPOISQ\user1 <jjs991217@gmail.com>
2023-12-29 18:13:07 +0900

    통합본 ver 0.4.1

--- src/com/kh/jobpost/model/dao/JobPostDao.java
+++ src/com/kh/jobpost/model/dao/JobPostDao.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 
 import com.kh.jobpost.model.vo.JobPost;
 
-public class PostDao {
+public class JobPostDao {
 
 	public JobPost detailBoard(SqlSession session, int jobpostNo) {
 		return session.selectOne("post-mapper.detailPost", jobpostNo);
